005e04bd5300735423c9284c3f8dd9c0601e56ac
Mat <matiastejerina94@gmail.com>
2023-10-28 21:53:18 -0300

    dta

---
+++ src/layouts/JoinLayout/JoinLayout.data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        icon: "truck",
+        title: "Gestiona tus pedidos",
+        description: "Ten el control de todos tus pedidos y recibe notificaciones con el seguimiento."
+    },
+    {
+        icon: "wechat",
+        title: "Atención al cliented",
+        description: "Tenemos un agente para atenderte las 24 horas del día, los 7 días de la semana."
+    }
+]
