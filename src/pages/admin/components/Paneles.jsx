8d5e8a29bafa8afb9f9d12bad8658c64d1253f74
Mat <matiastejerina94@gmail.com>
2023-11-22 16:29:51 -0300

    panel de admin

--- src/pages/admin/components/Paneles.jsx
+++ src/pages/admin/components/Paneles.jsx
@@ -1,17 +1,17 @@
 import { PanelesData } from "./dataPaneles"
 import ViewUser from "@/layouts/ViewUser/ViewUser"
-import { Search } from "@/layouts/ViewUser/components"
-import Link from "next/link"
 import { useEffect, useState } from "react"
 
 
 export default function Paneles() {
+    const [show, setShow] = useState({})
 
     useEffect(() => {
         showCategories()
+
     }, [])
 
-    const [show, setShow] = useState({})
+
 
     const showCategories = () => {
         const categories = {}
@@ -20,25 +20,27 @@ export default function Paneles() {
         })
         setShow(categories)
     }
-    console.log(show)
 
-    const handleShow = (paneles, button) => {
-        paneles.filter((panel) => {
-            if (panel.menuItem === button.target.name) {
-                return setShow(true)
+    const handleShow = (buttonName) => {
+        const keysShow = Object.keys(show)
+        keysShow.forEach(key => {
+            if (key === buttonName) {
+                show[key] = true && !buttonName === false
             }
         })
+        console.log(show)
 
-    }
+    };
+
 
     return (
         <ViewUser>
-            <div className=" flex ml-10 mt-10 gap-1">
+            <div className=" flex ml-10 mt-10">
                 {PanelesData.map((panel, index) =>
                     <>
-                        <button onClick={(e) => handleShow(PanelesData, e)} name={panel.menuItem} key={index} className="flex flex-col items-center gap-2 border rounded-t-md px-4 py-2">
+                        <button onClick={(e) => handleShow(e.target.name)} name={panel.menuItem} key={index} className="flex flex-col items-center gap-2 border rounded-t-md px-4 py-2">
                             {panel?.menuItem}
-                            <span>{typeof panel.icon === "function" ? panel.icon() : ""}</span>
+
                         </button>
                     </>
                 )}
