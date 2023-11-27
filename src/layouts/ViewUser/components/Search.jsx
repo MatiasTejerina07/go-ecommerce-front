62ff110fd065974e32efcc433ebb655598f2faf5
Mat <matiastejerina94@gmail.com>
2023-11-27 14:36:58 -0300

    search

--- src/layouts/ViewUser/components/Search.jsx
+++ src/layouts/ViewUser/components/Search.jsx
@@ -1,10 +1,36 @@
 import { Input } from "@nextui-org/react"
 import { SearchIn } from "@/icon/icons"
+import { useRouter } from "next/router"
 
-export default function Search({ className }) {
+
+export default function Search({ className, queryName = "search" }) {
+    const router = useRouter();
+    const inputValue = router.query[queryName] || "";
+
+
+    const onChange = (data) => {
+        if (data.target.value) {
+            router.replace({ query: { ...router.query, [queryName]: data.target.value } })
+        } else {
+            clearSearch()
+        }
+
+    }
+
+    const clearSearch = () => {
+        const newQuery = router.query;
+        delete newQuery[queryName]
+        router.replace({ query: newQuery })
+    }
+
     return (
         <div>
-            <Input title="hola" placeholder="Buscar" endContent={<SearchIn />} className={className} variant="flat" />
+            <Input value={inputValue}
+                title="hola"
+                onChange={onChange}
+                placeholder="Buscar"
+                endContent={<SearchIn />}
+                className={className} variant="flat" />
         </div>
     )
 }
--- src/pages/admin/components/Products.jsx
+++ src/pages/admin/components/Products.jsx
@@ -5,20 +5,23 @@ import { productsCtrl } from "@/api"
 import LoadingData from "./LoadingData";
 import TableList from "./Table";
 import Pagination from "./Pagination";
+import { useRouter } from "next/router";
 
 const ITEMS_PER_PAGE = 10;
 
 export default function Products() {
-
+    const router = useRouter();
+    const { query } = router;
     const [products, setProducts] = useState(null)
     const [totalPage, setTotalPage] = useState(null)
-    const page = 1
+    const page = Number(query.page) || 1;
 
 
     useEffect(() => {
         (async () => {
             try {
-                const response = await productsCtrl.getAll(1, 10, "")
+                const searchText = query.searchAdmin || ""
+                const response = await productsCtrl.getAll(1, 10, searchText)
                 setProducts(response.data || [])
                 const total = Math.ceil(response.totalItems / ITEMS_PER_PAGE)
                 setTotalPage(total)
@@ -26,14 +29,14 @@ export default function Products() {
                 console.log(error)
             }
         })()
-    }, [])
+    }, [query.page, query.searchAdmin])
 
     if (!products) return <LoadingData title="Cargando productos" className="-translate-x-1/2 top-1/2 left-1/2" />;
 
     return (
         <div className="w-full h-full">
             <div className="flex justify-between">
-                <Search className={"w-[300px]"} />
+                <Search queryName="searchAdmin" className={"w-[300px]"} />
                 <ButtonAdd name="Add Product" />
             </div>
             <div className="mt-4 ">
